7e7087ca253f713feb83ce3293e81010db1283e4
Mickysaurio-rex <mickymolina@gmail.com>
2022-05-27 04:48:39 -0400

    Ejercicios

--- PatronesDis2022/src/main/java/interpreter/practica/numerosRomanos/Client.java
+++ PatronesDis2022/src/main/java/interpreter/practica/numerosRomanos/Client.java
@@ -4,7 +4,7 @@ package interpreter.practica.numerosRomanos;
 public class Client {
     public static void main (String[]args){
 
-        String msg=" 1 2 3 4";
+        String msg=" 1 2 3 4 5 6 7 8 9 10";
 
         Interpreter parser = new Interpreter(msg);
         System.out.println("msg interpretado: "+parser.evaluateMsg());
--- PatronesDis2022/src/main/java/interpreter/practica/numerosRomanos/RomanoI.java
+++ PatronesDis2022/src/main/java/interpreter/practica/numerosRomanos/RomanoI.java
@@ -5,9 +5,14 @@ public class RomanoI extends RomanosT{
 
     @Override
     public void interpreter(Context context) {
-            if (context.input.startsWith("1")){
-                context.output= context.output+" I";
-                context.input= context.input.substring(1);
+        if (context.input.startsWith("1")){
+            context.input= context.input.substring(1);
+                if (context.input.startsWith("0")){
+                    context.output= context.output+" X";
+                    context.input= context.input.substring(1);
+                }else{
+                    context.output= context.output+" I";
+                }
         }
 
     }
