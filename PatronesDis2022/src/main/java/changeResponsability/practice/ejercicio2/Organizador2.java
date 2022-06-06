e8f1434020140af95fef87edc1246996ec5290d0
Mickysaurio-rex <mickymolina@gmail.com>
2022-06-06 01:42:27 -0400

    Change Responsability

--- PatronesDis2022/src/main/java/changeResponsability/practice/ejercicio2/Organizador2.java
+++ PatronesDis2022/src/main/java/changeResponsability/practice/ejercicio2/Organizador2.java
@@ -17,7 +17,7 @@ public class Organizador2 implements IOrganizador{
             for (int j = 1; j < personas.length ; j++) {
                 aux = personas[j].getEdad();
                 i = j-1;
-                while(i>-1 && personas[i].getEdad()>aux){
+                while(i>=0 && personas[i].getEdad()>aux){   // 9,4,5,12,1
                     personas[i+1] = personas[i];
                     i = i-1;
                 }
--- PatronesDis2022/src/main/java/changeResponsability/practice/ejercicio2/Organizador3.java
+++ PatronesDis2022/src/main/java/changeResponsability/practice/ejercicio2/Organizador3.java
@@ -12,16 +12,24 @@ public class Organizador3 implements IOrganizador{
     @Override
     public void criteriaHandler(Persona[] personas) {
         if(personas.length>50){
-            Persona aux;
-            for (int i = 0; i < personas.length - 1; i++) {
-                for (int j = 0; j < personas.length - i - 1; j++) {
-                    if (personas[j + 1].getEdad() < personas[j].getEdad()) {
-                        aux = personas[j + 1];
-                        personas[j + 1] = personas[j];
-                        personas[j] = aux;
+            for (int i = 0; i < personas.length - 1; i++)
+            {
+                int min = i;
+                for (int j = i + 1; j < personas.length; j++)
+                {
+                    if (personas[j].getEdad() < personas[min].getEdad())
+                    {
+                        min = j;
                     }
                 }
+                if (i != min)
+                {
+                    Persona aux= personas[i];
+                    personas[i] = personas[min];
+                    personas[min] = aux;
+                }
             }
+
         }else{
             next.criteriaHandler(personas);
         }
